f548bfc34a2c6efa135c197e95af6aae8c38180b
collins ihezie <collinsihezie6@gmail.com>
2023-01-02 08:05:23 +0100

    fixed

--- pages/api/form.ts
+++ pages/api/form.ts
@@ -25,19 +25,27 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse<
 
     const transport = await createNodeMailerTransport()
 
-    transport.verify((error, success) => {
-        if(error){
-            console.log(error)
+    transport.sendMail(mailDetails, (err, data) => {
+        if(err) {
+            console.log(err);
         } else {
-            transport.sendMail(mailDetails, (err, data) => {
-                if(err) {
-                    console.log(err);
-                } else {
-                    console.log(data);
-                }
-            });
+            console.log(data);
         }
     })
+
+    // transport.verify((error, success) => {
+    //     if(error){
+    //         console.log(error)
+    //     } else {
+    //         transport.sendMail(mailDetails, (err, data) => {
+    //             if(err) {
+    //                 console.log(err);
+    //             } else {
+    //                 console.log(data);
+    //             }
+    //         });
+    //     }
+    // })
 
     res.status(200).json('success')
 }
